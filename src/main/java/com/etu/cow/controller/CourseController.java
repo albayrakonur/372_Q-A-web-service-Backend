c9e7ce09063d77333732013676ca0d295bcd1716
Onur Albayrak <onuralbayrak@etu.edu.tr>
2019-07-26 05:08:21 +0300

    fixed method type issue

--- src/main/java/com/etu/cow/controller/CourseController.java
+++ src/main/java/com/etu/cow/controller/CourseController.java
@@ -26,7 +26,7 @@ public class CourseController {
         return new ResponseEntity<>(courseList, HttpStatus.OK);
     }
 
-    @GetMapping("/courses/getCourse")
+    @PostMapping("/courses/getCourse")
     public ResponseEntity<Course> getCourse(@RequestParam String courseCode) {
         Course result = courseRepository.findByCourseCode(courseCode);
         if (result == null) {
